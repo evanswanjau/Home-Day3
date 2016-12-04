306dfc331b5d1fe8d1c0e257425a2fbdfb073e5f
Evans Wanjau <evanswanjau@gmail.com>
2016-12-04 17:23:20 +0300

    Changes made to binary searc file(while iteration)

--- Home-Day3/Andelabs/binarysearch.py
+++ Home-Day3/Andelabs/binarysearch.py
@@ -1,9 +1,25 @@
-class BinarySearch(list):
+class BinarySearch(the_list):
   __init__(a,b):
-    length = len(list)
+    length = len(the_list)
     minimum = 0
-  def search(target):
-      median = minimum - (maximum - minimum) // 2
-      if target == median:
-        
-        
+    maximum = len(the_list) - 1
+    return length
+
+def search(index):
+    while minimun < maximum:
+        median = (minimum + maximum)//2
+        if index == the_list[median]:
+        x = {index}
+        return x
+      elif index < the_list[median]:
+        for index in range(minimum:median):
+          index -= b
+          x = {count, index}
+          return x
+      elif index > the_list[median]:
+        for index in range(median:maximum):
+          index += b
+          x = {count, index}
+          return x
+      else:
+        return index, 'not found'
