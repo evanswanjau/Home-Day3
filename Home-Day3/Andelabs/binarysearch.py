cabe5a4a0094eae5b43a36b5be26f96ac778ca6e
Evans Wanjau <evanswanjau@gmail.com>
2016-12-04 16:31:26 +0300

    Adding the Binary search file(Last Home bootcamp program)

---
+++ Home-Day3/Andelabs/binarysearch.py
@@ -0,0 +1,9 @@
+class BinarySearch(list):
+  __init__(a,b):
+    length = len(list)
+    minimum = 0
+  def search(target):
+      median = minimum - (maximum - minimum) // 2
+      if target == median:
+        
+        
